8c6a524ed84a5c4f7bc7fc75cef122ea35bf51d3
PolinaKrasovskaya <polina.vorotnik@gmail.com>
2022-02-01 02:27:21 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import User from './components/User';
+import user from './user.json';
+
+export default function App() {
+  return <div>
+    <User 
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      followers={user.stats.followers}
+      views={user.stats.views}
+      likes={user.stats.likes}
+    />, 
+  </div>;
+}
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
